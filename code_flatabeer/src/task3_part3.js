cd2cea80a9041af83d3fca379a9f8ef6b409c580
Martinpouh <martinchege2015@gmail.com>
2022-06-21 17:39:12 +0300

    test

--- code_flatabeer/src/task3_part3.js
+++ code_flatabeer/src/task3_part3.js
@@ -7,25 +7,25 @@ const beerList = document.getElementById('beer-list')
 const url = 'http://localhost:3000/beers';
 
 fetch(url).then((response) => response.json())
-.then(beers =>{
-  beers.forEach(beer => {
-    let listR = document.createElement('li');
-    listR.textContent = beer.name;
-    beerList.appendChild(listR);
-  })
-beerName.innerHTML = beers[6].name;
-  beerImage.src = beers[6].image_url;
-  beerDescription.innerHTML = beers[6].description;
-  beers[6].reviews.forEach((review) =>{
-    let listR = document.createElement('li');
-    listR.textContent = review;
-    beerReviews.appendChild(listR);
-  })
-})
-document.querySelector('#review-form').addEventListener('submit', (e)=>{
-  e.preventDefault();
- let text = document.querySelector('#review-form textarea')
- let listReveiw =document.createElement('li');
- listReveiw.innerText = text.value;
- beerReviews.appendChild(listReveiw);
+    .then(beers => {
+        beers.forEach(beer => {
+            let listR = document.createElement('li');
+            listR.textContent = beer.name;
+            beerList.appendChild(listR);
+        })
+        beerName.innerHTML = beers[6].name;
+        beerImage.src = beers[6].image_url;
+        beerDescription.innerHTML = beers[6].description;
+        beers[6].reviews.forEach((review) => {
+            let listR = document.createElement('li');
+            listR.textContent = review;
+            beerReviews.appendChild(listR);
+        })
+    })
+document.querySelector('#review-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let text = document.querySelector('#review-form textarea')
+    let listReveiw = document.createElement('li');
+    listReveiw.innerText = text.value;
+    beerReviews.appendChild(listReveiw);
 })
